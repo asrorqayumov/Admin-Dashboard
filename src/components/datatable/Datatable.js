037b56de2c30a2ef3784313a76707d0f554d8408
Asror Qayumov <qayumovasror2003@gmail.com>
2022-10-12 23:09:54 +0500

    :rocket: datatable finished

--- src/components/datatable/Datatable.js
+++ src/components/datatable/Datatable.js
@@ -4,14 +4,28 @@ import { DataGrid } from "@mui/x-data-grid";
 import { UserColumns, UserRows } from "../../datatablesource";
 
 export const Datatable = () => {
+
+  const actionColumn = [{
+    field:'action',
+    headerName:'Action',
+    width:200,
+    renderCell:()=>{
+      return(
+        <div className="cellAction">
+          <div className="viewButton">View</div>
+          <div className="deleteButton">Delete</div>
+        </div>
+      )
+    }
+  }];
+
   return (
     <div className="datatable">
-      {" "}
       <DataGrid
         rows={UserRows}
-        columns={UserColumns}
-        pageSize={5}
-        rowsPerPageOptions={[5]}
+        columns={UserColumns.concat(actionColumn)}
+        pageSize={9}
+        rowsPerPageOptions={[9]}
         checkboxSelection
       />
     </div>
