fcbee2e66a4cafba522088baf705976a720ea382
Asror Qayumov <qayumovasror2003@gmail.com>
2022-10-13 22:06:53 +0500

    :construction: single page in progress

--- src/pages/single/Single.js
+++ src/pages/single/Single.js
@@ -1,8 +1,26 @@
 import React from 'react';
 import './single.scss';
+import { Sidebar } from './../../components/sidebar/Sidebar';
+import { Navbar } from './../../components/navbar/Navbar';
 
 export const Single = () => {
   return (
-    <div>Single</div>
+    <div className='single'>
+      <Sidebar />
+      <div className="singleContainer">
+        <Navbar />
+       <div className="top">
+        <div className="left">
+          <h1 className="title">Information</h1>
+          <div className="item">
+            <img src="https://images.pexels.com/photos/977796/pexels-photo-977796.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="" className="itemImg" />
+            details
+          </div>
+        </div>
+        <div className="right"></div>
+       </div>
+       <div className="bottom"></div>
+      </div>
+    </div>
   )
 }
